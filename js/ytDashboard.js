f9bfae3161f83cb5a99119f5917f6cd19462aa57
ewuresi-amoo-adare <eamooadare3@gatech.edu>
2025-10-17 12:40:05 -0400

    added in drawing areas

--- js/ytDashboard.js
+++ js/ytDashboard.js
@@ -6,5 +6,24 @@ class YtDashboard{
         this.initVis()
     }
 
-    initVis() {}
+    initVis() {
+        let vis = this;
+
+        vis.margin = {top: 10, right: 50, bottom: 10, left: 50};
+        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
+        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+
+        vis.svg = d3.select("#" + vis.parentElement).append("svg")
+            .attr("width", vis.width + vis.margin.left + vis.margin.right)
+            .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+        vis.svg.append('g')
+            .attr('class', 'title dash-title')
+            .append('text')
+            .text('Top Youtube Categories')
+            .attr('transform', `translate(${vis.width / 2}, 20)`)
+            .attr('text-anchor', 'middle');
+    }
 }
